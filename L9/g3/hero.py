c5dcc5a17e37db26e54e933731e6712f4265833b
Roman <romanvytvytskyy@yahoo.com>
2022-11-23 10:34:58 +0200

    2022-11-23

--- L9/g3/hero.py
+++ L9/g3/hero.py
@@ -1,3 +1,7 @@
+from random import randint
+from time import sleep
+
+
 class Hero():
     def __init__(self, name, health, power, armor, weapon):
         self.name = name
@@ -8,26 +12,57 @@ class Hero():
 
     def print_info(self):
         print("New hero")
+        sleep(1)
         print("Hero name is", self.name)
+        sleep(1)
         print("Health:", self.health)
+        sleep(1)
         print("Armor:", self.armor)
+        sleep(1)
         print("Weapon:", self.weapon)
+        sleep(1)
 
     def strike(self, enemy):
         print("Hit!", self.name, "attacking", enemy.name)
+        sleep(1)
         enemy.armor = enemy.armor - self.power
         if enemy.armor <= 0:
             enemy.health = enemy.health + enemy.armor
             enemy.armor = 0
         print(enemy.name, "armor:", enemy.armor, "and health:", enemy.health)
+        sleep(1)
 
     def fight(self, enemy):
         while self.health > 0 or enemy.health > 0:
             self.strike(enemy)
             if enemy.health <= 0:
                 print(enemy.name, "felt in battle")
+                sleep(1)
                 break
             enemy.strike(self)
             if self.health <= 0:
                 print(self.name, "felt in battle")
+                sleep(1)
                 break
+
+
+class Sponge(Hero):
+    def __init__(self, name, health, power, armor, weapon):
+        super().__init__(name, health, power, armor, weapon)
+
+    def strike(self, enemy):
+        print("Blow!!!", self.name, "attacking", enemy.name)
+        sleep(1)
+        enemy.armor = enemy.armor - randint(0, self.power)
+        if enemy.armor <= 0:
+            enemy.health = enemy.health + enemy.armor
+            enemy.armor = 0
+            if randint(0, 100) > 50:
+                self.health = self.health + randint(0, 5)
+        print(enemy.name, "armor:", enemy.armor, "and health:", enemy.health)
+        sleep(1)
+
+
+class MyClass(Hero):
+    # TODO Придумати і описати свій клас з одним унікальним методом
+    pass
--- L9/g3/main.py
+++ L9/g3/main.py
@@ -1,7 +1,7 @@
-from hero import Hero
+from hero import Hero, Sponge
 
 hero1 = Hero("Patrick star", 50, 1, 5, "Brain storm")
-hero2 = Hero("Sponge Bob", 50, 5, 10, 'Soap buble')
+hero2 = Sponge("Sponge Bob", 50, 5, 10, 'Soap buble')
 
 hero1.print_info()
 hero2.print_info()
